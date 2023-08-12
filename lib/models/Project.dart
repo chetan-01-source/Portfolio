637ef94d1874e18f766edf8673e01d9b42b51cfe
chetan-01-source <chetanmarathe1000@gmail.com>
2023-08-12 11:05:42 +0530

    deployed successfully

---
+++ lib/models/Project.dart
@@ -0,0 +1,48 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "TO DO APP- React App",
+    description:
+    "Users need to aware about their daily task which they got forgot when working with other activities,So I created TO DO App which helps users to store their task daily and complete it.",
+  ),
+  Project(
+    title: "Movie Complete App - Flutter Fully Responsive Design UI",
+    description:
+        "Continuously providing a live data of movies with the help of TMDB Api,which helps user to get aware information about movies.",
+  ),
+  Project(
+    title: "BMI App - Flutter UI",
+    description:
+        "This App helps user to check their body mass index which suggest what to do next with dark theme UI.",
+  ),
+  Project(
+    title: "FLASHCHAT chat/messaging app - Flutter  UI",
+    description:
+        "Today we gonna build messing/chat app #ui using #flutter that runs both Android and iOS devices. I create a group chat app.",
+  ),
+  Project(
+    title: "Clima the weather App - Flutter UI",
+    description:
+        "Today, knowing climate info is much moreneed for user,I created clima app which deals with api and sends climate of each city.",
+  ),
+  Project(
+    title: "Quizzler App ",
+    description:
+        "Quizzler App helps user to solve various quizzes which is helpful for childrens to improve themselves.",
+  ),
+  Project(
+    title: "Bitcoin Ticker App- Flutter UI",
+    description:
+        "It is important to get current rate of CryptoCurrency , I created Bitcoin Ticker App which helps user to covert CryptoCurrency into each state currency rate",
+  ),
+  Project(
+    title: "Responsive Portfolio -Flutter UI",
+    description:
+    "Responsive Flutter Portfolio helps to express yourself with a better way which shows your own creatvity and performance. ",
+  ),
+];
---
+++ lib/models/Recommendation.dart
@@ -0,0 +1,44 @@
+class Recommendation {
+  final String? name, source, text;
+
+  Recommendation({this.name, this.source, this.text});
+}
+
+final List<Recommendation> demo_recommendations = [
+  Recommendation(
+    name: "Sahil Vichare",
+    source: "Linkedin",
+    text:
+        "I have had the pleasure of working with Chetan on two flutter projects and what strikes me the most about his work is his ability to write clean and reusable codes with best practices. He is hardworking and makes it so easy to transcribe ideas into beautiful and testable flutter apps. He is a Flutter/Dart gem and has my highest recommendation",
+  ),
+  Recommendation(
+    name: "Sushant Jadhav",
+    source: "Linkedin",
+    text:
+        "Chetan is great asset for any organization. It's a true pleasure working with him at TakeIn. His flutter skill is amazing as well as his professionalism and being a good team player. Chetan's problem solving skill is also one of his great skills.",
+  ),
+  Recommendation(
+    name: "Neha Patil",
+    source: "Linkedin",
+    text:
+    "Chetan is great app developer I like to Learn and develope app by collabrating with you.",
+  ),
+  Recommendation(
+    name: "Uday Kaple",
+    source: "Linkedin",
+    text:
+        "I like your way you doing your project.",
+  ),
+  Recommendation(
+    name: "Yash Kirnapure",
+    source: "Linkedin",
+    text:
+        "Nice Apps! I will be like to work and collaborate with you for Upcoming projects.",
+  ),
+  Recommendation(
+    name: "Vaishnavi Nikam",
+    source: "Linkedin",
+    text:
+    "Nice Projects, I will use this project for exploring flutter technology.",
+  ),
+];
